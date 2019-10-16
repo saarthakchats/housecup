087eb95ca698b0848a92bdc2c8057ed4003b242b
Kartikeya Singh <kartikeya.singh08@gmail.com>
2019-10-16 19:30:13 +0530

    adding automatic ranking of houses

--- points/admin.py
+++ points/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import House
 
 # Register your models here.
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'points')
+admin.site.register(House, HouseAdmin)
--- points/views.py
+++ points/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import House
 
 # Create your views here.
 def home(request):
-    return render(request, 'points/home.html')
+	ranked = House.objects.order_by('points')
+	return render(request, 'points/home.html', {'first': ranked[3], 'second': ranked[2], 'third': ranked[1], 'fourth': ranked[0]})
